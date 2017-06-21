67d4724ae2bc57f59360ad5fa4f24eff8db264ee
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 18:04:42 +0000

    Initial commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,84 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+  end
+
+  # GET /blogs/new doesn't actually create a new blog cus it doesn't actually have any parameter; gives the ability to have the layout/making the form available: new blog, title, body
+  # it's mapped to the 'form' definition in file folder views, blogs, new.html.ert
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  # has the abilitity to know wat blog you want to edit, thats why it has it on the before_action
+  # makes the form available
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  # actually creates the blog, takes the 'form' parameter
+  # 
+  def create
+    @blog = Blog.new(blog_params)
+
+#format.html makes the new blog post you created avaliable in the browser
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+       # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+      #  format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  # does the actual connections with the database; does the hardwork and goes through and does all the saves/changes you have to the database
+  # whenever you see smthing like (blog_params) its either a variable or method
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+       # format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+      #  format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  # The way you delete a post
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+     # format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # going through and seeing what was submitted in the form and its making it avaliable to the update and new method(def update and def create @blog = Blog.new(blog_params) )
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
+end
